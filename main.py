f2d62a8c4d33da39495030ea272d2a5f23baab64
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-06-20 19:00:17 -0300

    refactor: make initial settings

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+Main script for the schedule builder.
+Refer to README for instructions.
+"""
+
 from scheduler import *
 from postprocessing import *
 import exporter
@@ -13,30 +18,39 @@ PERIOD_FOOD = construct_periods([
     '01214', '11214', '21214', '31214', '41214',
     '01719', '11719', '21719', '31719', '41719'
 ], WEEK)
-PERIOD_AFTERNOON = [
+PERIOD_AFTERNOON = construct_periods([
     '01417', '11417', '21417', '31417', '41417'
-]
+], WEEK)
+PERIOD_DAY = construct_periods([
+    '00922', '10922', '20922', '30922', '40922'
+], WEEK)
 
 # INITIAL SETTINGS
 QUERY = {
     'MAT237Y': ['MTH0910', 'MTH1112', 'MTH1415', 'MTH1718'],
     'MAT237T': ['T1011', 'T1112', 'T1213', 'T1314', 'T1415', 'T1516', 'T1617', 'T1718', 'T1819'],
-    'MAT244F': ['W0911F1011', 'W1113F1112', 'W1315F1415', 'W1517F1516'],
+    'MAT244H': ['W0911F1011', 'W1113F1112', 'W1315F1415', 'W1517F1516'],
     'MAT244T': ['M0910', 'M1011', 'M1112', 'M1213', 'M1314', 'M1415', 'M1516', 'M1617', 'M1718', 'M1819'],
-    'STA257F': ['M1113W1112', 'M1517W1516'],
+    'STA257H': ['M1113W1112', 'M1517W1516'],
     'STA257T': ['W1213', 'W1617', ''],
-    'CSC265F': ['MWF1516'],
-    'CSC207F': ['W1315', 'W1517', 'H1315', 'H1820'],
-    'CSC207T': ['H0911']
+    'CSC265H': ['MWF1516'],
+    'CSC207H': ['W1315', 'W1517', 'H1315', 'H1820'],
+    'CSC207T': ['H0911'],
+    'PHY250H': ['MW0910'],
+    'PHY250T': ['T1516', 'W1617', 'F1415', 'F1617']
 }
-
 
 POST_PROCESS_CHAIN: list[ChainEntry] = [
     (
-    lambda s: test_breaks(s, PERIOD_FOOD, 1),
-    lambda x: (sum(x.max_break_length) / len(x.max_break_length)) * -1,
-    lambda x: all(x.has_break)
+        lambda s: test_breaks(s, PERIOD_FOOD, 1),
+        lambda x: (sum(x.max_break_length) / len(x.max_break_length)) * -1,
+        lambda x: all(x.has_break)
     ),
+    (
+        lambda s: calc_breaks(s, PERIOD_DAY),
+        lambda x: x.num_chunks * -1,
+        None
+    )
 ]
 
 
@@ -85,16 +99,19 @@ def main():
 
     result = sort_chain.evaluate()
 
-    print("DISPLAYING RESULT")
+    print(f"DISPLAYING RESULT - {len(result)} results")
     for i in range(len(result)):
         print(i + 1)
         print(exporter.display_week_schedule(result[i][0]))
         if input() == "q":
             break
 
+    return result
 
+
+RESULT = []
 if __name__ == "__main__":
-    main()
+    RESULT = main()
 
 
 
